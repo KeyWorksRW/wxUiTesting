53d012d56188d5ab10c30e0992b0b28f9d505802
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2022-12-18 17:28:11 -0800

    Add template python files
    These are hand-created, not generated, and won't change as frequently.

---
+++ src/python/mainframe.py
@@ -0,0 +1,50 @@
+import wx
+
+import mainframe_base
+import python_dlg
+
+class mainframe(mainframe_base.MainFrameBase):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    # Event handler functions
+    def OnChoicebook(self, event):
+        event.Skip()
+
+    def OnCommonDialog(self, event):
+        event.Skip()
+
+    def OnImportTest(self, event):
+        event.Skip()
+
+    def OnListbook(self, event):
+        event.Skip()
+
+    def OnMultiTestDialog(self, event):
+        event.Skip()
+
+    def OnNotebook(self, event):
+        event.Skip()
+
+    def OnOtherCtrls(self, event):
+        event.Skip()
+
+    def OnQuit(self, event):
+        self.Close()
+
+    def OnRibbonDialog(self, event):
+        event.Skip()
+
+    def OnToolbook(self, event):
+        event.Skip()
+
+    def OnTreebook(self, event):
+        event.Skip()
+
+    def OnWizard(self, event):
+        event.Skip()
+
+    def OnPythonDlg(self, event):
+        dlg = python_dlg.python_dlg(self)
+        dlg.ShowModal()
+        dlg.Destroy()
