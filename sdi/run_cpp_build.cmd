cc06331080f12ad1bda5aa9a29a02749d0c619ef
Randalphwa <38287198+Randalphwa@users.noreply.github.com>
2025-05-01 07:16:06 -0700

    Fix location of wxUiEditor.exe
    build/stage/bin/debug was replaced with build/bin/Debug

--- sdi/run_cpp_build.cmd
+++ sdi/run_cpp_build.cmd
@@ -4,7 +4,7 @@
 @REM is in the ..\..\build\stage\bin\debug directory.
 
 echo Generating C++ files...
-..\..\wxUiEditor\build\stage\bin\debug\wxUiEditor.exe --gen_cpp sdi_test.wxui
+..\..\wxUiEditor\build\bin\Debug\wxUiEditor.exe --gen_cpp sdi_test.wxui
 type sdi_test.log
 cmake.exe --build build --config Debug --target sdi_test
 ..\wxWidgets32\build\Debug\sdi_test.exe
